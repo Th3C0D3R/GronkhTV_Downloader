2aa6349f1bd89bb9212b4d7c507b714c65ca5e4a
TH3C0D3R <7798500+Th3C0D3R@users.noreply.github.com>
2024-01-16 20:32:06 +0100

    update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,78 +8,87 @@ using Newtonsoft.Json;
 
 namespace GronkhTV_DL
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
 
-            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
-            InitWebDriver();
-            DataContext = this;
-        }
+			AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+		}
+		private List<Video> CollectStreams()
+		{
+			try
+			{
+				StreamsLoaded = false;
 
-        private void CurrentDomain_ProcessExit(object? sender, EventArgs e)
-        {
-            try
-            {
-                webDriver.Close();
-                webDriver.Quit();
-            }
-            catch (Exception)
-            {}
-        }
-        private void InitWebDriver()
-        {
-            try
-            {
-                var s = DriverFinder.FullPath(new ChromeOptions() { BrowserVersion = "stable" });
-                var driverService = ChromeDriverService.CreateDefaultService(s);
-                driverService.HideCommandPromptWindow = true;
-                var options = new ChromeOptions();
-                options.AddArguments("--headless", "--no-sandbox", "--disable-gpu", "disable-gpu");
-                webDriver = new ChromeDriver(driverService,options);
-            }
-            catch (Exception)
-            { }
-        }
-        private List<Video> CollectStreams()
-        {
-            try
-            {
-                StreamsLoaded = false;
+				webDriver.Navigate().GoToUrl("https://gronkh.tv/streams/");
+				jsExecuter = (IJavaScriptExecutor)webDriver;
 
-                webDriver.Navigate().GoToUrl("https://gronkh.tv/streams/");
-                jsExecuter = (IJavaScriptExecutor)webDriver;
+				string x = (string)jsExecuter.ExecuteAsyncScript(Properties.Resources.fetchStreams);
 
-                string x = (string)jsExecuter.ExecuteAsyncScript(Properties.Resources.fetchStreams);
+				Root? root = JsonConvert.DeserializeObject<Root>(x);
+				if (root == null) return [];
 
-                Root? root = JsonConvert.DeserializeObject<Root>(x);
-                if (root == null) return [];
+				List<Video>? videos = root.results?.videos?.OrderBy(v => v.episode).Reverse().ToList();
+				if (videos == null) return [];
 
-                List<Video>? videos = root.results?.videos?.OrderBy(v => v.episode).Reverse().ToList();
-                if (videos == null) return [];
+				StreamsLoaded = true;
+				return videos;
+			}
+			catch (Exception)
+			{
+				return [];
+			}
+		}
 
-                StreamsLoaded = true;
-                return videos;
-            }
-            catch (Exception)
-            {
-                return [];
-            }
-        }
+		private void miReload_Click(object sender, RoutedEventArgs e)
+		{
 
-        private void miReload_Click(object sender, RoutedEventArgs e)
-        {
+		}
 
-        }
 
-        private void miExit_Click(object sender, RoutedEventArgs e)
-        {
-            Environment.Exit(0);
-        }
-    }
+		private void CurrentDomain_ProcessExit(object? sender, EventArgs e)
+		{
+			DeInitWebDriver();
+		}
+		private void InitWebDriver()
+		{
+			try
+			{
+				var s = DriverFinder.FullPath(new ChromeOptions() { BrowserVersion = "stable" });
+				var driverService = ChromeDriverService.CreateDefaultService(s);
+				driverService.HideCommandPromptWindow = true;
+				var options = new ChromeOptions();
+				options.AddArguments("--headless", "--no-sandbox", "--disable-gpu", "disable-gpu");
+				webDriver = new ChromeDriver(driverService, options);
+			}
+			catch (Exception)
+			{ }
+		}
+		private void DeInitWebDriver()
+		{
+			try
+			{
+				webDriver.Close();
+				webDriver.Quit();
+			}
+			catch (Exception)
+			{ }
+		}
+		private void miExit_Click(object sender, RoutedEventArgs e)
+		{
+			DeInitWebDriver();
+			Environment.Exit(0);
+		}
+
+		private void frmMain_Loaded(object sender, RoutedEventArgs e)
+		{
+			InitWebDriver();
+			DataContext = this;
+		}
+	}
 }
